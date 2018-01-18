c66a4ba763d1859f00f576b11a788355eafacd13
Hack Reactor Student <student@hackreactor.com>
2018-01-18 09:59:09 -0800

    completed

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,30 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = 0;
   }
 
+enqueue(value){
+this.count += 1;
+this.storage[this.count - 1] = value;
 }
+
+dequeue() {
+var FIFO = this.storage[1];
+this.count -= 1;
+
+for (let i = 1; i < this.count; i++) {
+  this.storage[i] = this.storage[i + 1]; //current guy leaves, the guy behind him needs to take over his queue #.
+}
+return FIFO;
+}
+
+size() {
+if(this.count < 1 || this.count === 0) {
+return 0;
+}
+
+}
+
+var queue = new Queue();
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,27 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = 0;
+  }
+
+  push() {
+  this.count += 1;
+  this.storage[this.count] = value;
+  }
+
+  pop() {
+    this.count -= 1;
+    return this.storage[this.count + 1];
+  }
+
+  size() {
+    if(this.count < 0) {
+  return 0;
+}
+return this.count;
   }
 
 }
+
+var stack = new Stack()
